ba18be72a85b91bfcfdbd6b82d11b603da6193ad
evanshi0811 <evanshi@LAPTOP-JJ9BMRHA>
2024-03-26 17:52:51 -0400

    ehh

--- js/script.js
+++ js/script.js
@@ -3,20 +3,35 @@ let pokemonList = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","C
 // Answers
 
 // Question 1
+document.querySelector("#host-name").innerHTML = "Evan";
 
-
-
+document.querySelector("#collaborator-name").innerHTML = "Flora";
 
 // Question 2
 // You can also create their own CSS classes in style.css file and use that. 
+let hidebutton = document.querySelector("#instruction_button");
+hidebutton.addEventListener("click", hideInstructions);
 
-
+function hideInstructions() {
+    let questions = document.querySelectorAll(".question"); 
+    for(let i = 0; i < questions.length; i++){
+        questions[i].style.display = "none";
+    }
+}
 
 
 // Question 3
 // Try change event first
 // There is another event which fires for every character. This is what we want to use.
-
+let firstName = document.getElementById("first_name");
+firstName.addEventListener('change', function() {
+    let x = document.getElementById("first_name").value;
+    let text;
+    if (x == "John") {
+        text = "Doe";
+    }
+    document.getElementById("last_name").innerHTML = text;
+});
 
 
 // Question 4
@@ -28,6 +43,17 @@ let pokemonList = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","C
 // Question 5
 // Try change event first
 // There is another event which fires for every character. This is what we want to use.
+function match(p1, p2) {
+    if (p1 != p2) { 
+        document.querySelector("#password-alert").innerHTML = "Password does not match";
+    } 
+    else{
+        document.querySelector("#password-alert").innerHTML = "Password Matches";
+    }
+}
+let p1 = document.querySelector("#password");
+let p2 = document.querySelector("#confirm_password");
+document.querySelector("#password-alert").addEventListener("onkeyup", match(p1.value, p2.value));
 
 
 
